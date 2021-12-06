c990282bbcdc341e6a975612f3898768fce31789
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2021-12-06 14:01:17 -0500

    Add files via upload

--- play_nine_player.py
+++ play_nine_player.py
@@ -1,4 +1,5 @@
-# An example player agent for the
+# An example player agent for the Play Nine Solitaire project.
+# Version December 6, 2021, Ilkka Kokkarinen
 
 # Returns a tuple of two strings, the name and the student ID of the author.
 
@@ -8,7 +9,7 @@ def get_author_info():
 
 # Choose the drawing action for the current draw.
 
-def choose_drawing_action(top_concealed, bottom_concealed, draws, kitty_card):
+def choose_drawing_action(top_concealed, bottom_concealed, draws_left, kitty_card):
     action = "xxx"
     while len(action) != 1 or action not in "kdKD":
         action = input("Will you take the (k)itty card or (d)raw from deck? ")
@@ -21,7 +22,7 @@ def choose_drawing_action(top_concealed, bottom_concealed, draws, kitty_card):
 # - row is the row number of the card subject to chosen action
 # - column is the column number of the card subject to chosen action
 
-def choose_replacement_action(top_concealed, bottom_concealed, draws, current_card):
+def choose_replacement_action(top_concealed, bottom_concealed, draws_left, current_card):
     action = "xxx"
     while len(action) != 1 or action not in "rRtT":
         action = input(f"Will you (r)eplace or (t)urn over a board card? ")
--- play_nine_runner.py
+++ play_nine_runner.py
@@ -5,7 +5,7 @@ import play_nine_player as pnp
 # -----------------------------------------------------------------------------------
 
 # How many hands are played in total to compute the final score.
-TOTAL_HANDS = 10
+TOTAL_HANDS = 1
 
 # Whether the hand actions are printed out, instead of just the final score.
 VERBOSE = True
@@ -26,6 +26,7 @@ DECK_FREQ = [-5, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10,
 # The frequencies of cards as the top card of the kitty.
 KITTY_FREQ = [5, 0, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12]
 
+
 # Compute the score for the finished hand.
 
 def score_hand(top_cards, bottom_cards):
@@ -33,20 +34,25 @@ def score_hand(top_cards, bottom_cards):
     pos_match = dict()
     for (t, b) in zip(top_cards, bottom_cards):
         if t != b:
-           total += (t + b)
+            total += (t + b)
         else:
             pos_match[t] = pos_match.get(t, 0) + 1
             total += -10 if t == -5 else 0
     for value in pos_match:
         count = pos_match[value]
-        total -= 5 * count if count > 1 else 0
+        mul = -5 if value == -5 else -5
+        total += mul * count if count > 1 else 0
     return total
+
 
 # Some examples of hand scoring.
 
+assert score_hand([4, 3, 12, 6], [4, 1, 12, 6]) == 4
 assert score_hand([4, 4, 2, 10], [4, 4, -5, 9]) == 6
-assert score_hand([-5, 3, -5, 1], [-5, 8, -5, 12]) == -6
 assert score_hand([5, 2, 12, 4], [8, 2, 3, 1]) == 33
+assert score_hand([1, -5, 10, 2], [6, -5, 10, 3]) == 2
+assert score_hand([6, 2, 6, 1], [6, 9, 6, 3]) == 5
+assert score_hand([2, -5, -5, 4], [10, -5, -5, 5]) == -9
 
 
 # Create a masked version of the cards in the hand, based on what has been revealed.
@@ -54,7 +60,7 @@ assert score_hand([5, 2, 12, 4], [8, 2, 3, 1]) == 33
 def conceal_cards(top_cards, bottom_cards, top_revealed, bottom_revealed):
     top_row = [card if reveal else '*' for (card, reveal) in zip(top_cards, top_revealed)]
     bottom_row = [card if reveal else '*' for (card, reveal) in zip(bottom_cards, bottom_revealed)]
-    return (top_row, bottom_row)
+    return top_row, bottom_row
 
 
 # Print out the current state of the hand.
@@ -63,13 +69,13 @@ def output_cards(top_cards, bottom_cards, top_revealed, bottom_revealed):
     top_row, bottom_row = conceal_cards(top_cards, bottom_cards, top_revealed, bottom_revealed)
     top_print = " ".join(f"{str(card):>3}" for card in top_row)
     bottom_print = " ".join(f"{str(card):>3}" for card in bottom_row)
-    print(f"Row 0: [{top_print}]")
-    print(f"Row 1: [{bottom_print}]")
+    print(f"Row 0: [{top_print} ]")
+    print(f"Row 1: [{bottom_print} ]")
 
 
 # Play one hand with the given rng, with n cards per row for a given number of draws.
 
-def play_one_hand(rng, columns, draws):
+def play_one_hand(rng, columns, draws_left):
     top_cards = [rng.choice(DECK_FREQ) for _ in range(columns)]
     bottom_cards = [rng.choice(DECK_FREQ) for _ in range(columns)]
     top_revealed = [False for _ in range(columns)]
@@ -77,32 +83,40 @@ def play_one_hand(rng, columns, draws):
     top_revealed[rng.choice(range(columns))] = True
     bottom_revealed[rng.choice(range(columns))] = True
 
-    # Keep playing as there are draws left and some cards are still face down.
-    while draws > 0 and (False in top_revealed or False in bottom_revealed):
+    # Keep playing as long as there are draws left and some cards are still face down.
+    while draws_left > 0 and (False in top_revealed or False in bottom_revealed):
         kitty_card = rng.choice(KITTY_FREQ)
         if VERBOSE:
-            draw_string = f"are {draws} draws" if draws > 1 else "is one draw"
+            draw_string = f"are {draws_left} draws" if draws_left > 1 else "is one draw"
             print(f"There {draw_string} remaining. Kitty card is {kitty_card}.")
             output_cards(top_cards, bottom_cards, top_revealed, bottom_revealed)
         top_concealed, bottom_concealed = conceal_cards(top_cards, bottom_cards, top_revealed, bottom_revealed)
-        action = pnp.choose_drawing_action(top_concealed, bottom_concealed, draws, kitty_card)
-        assert len(action) == 1 and action in "kKdR"
-        draws -= 1
+        action = pnp.choose_drawing_action(top_concealed, bottom_concealed, draws_left, kitty_card)
+        assert len(action) == 1 and action in "kKdR", f"Illegal action {action}"
+        draws_left -= 1
         current_card = kitty_card if action in "kK" else rng.choice(DECK_FREQ)
         if VERBOSE:
             print(f"You are holding a {current_card}.")
-        (action, row, column) = pnp.choose_replacement_action(top_concealed, bottom_concealed, draws, current_card)
-        assert row in range(2) and column in range(columns)
-        if action in "rR":
+        (action, row, column) = pnp.choose_replacement_action(top_concealed, bottom_concealed, draws_left, current_card)
+        assert row in range(2), f"Illegal row {row}"
+        assert column in range(columns), f"Illegal column {column}"
+        if action in "tT":  # You can only turn over cards that were face down.
+            if row == 0:
+                assert not top_revealed[column], f"Card at {row}, {column} already face up"
+            else:
+                assert not bottom_revealed[column], f"Card at {row}, {column} already face up"
+        if action in "rR":  # Any card on the board can be replaced, be it either face up or face down.
             if row == 0:
                 top_cards[column] = current_card
             else:
                 bottom_cards[column] = current_card
-        if row == 0:
+        if row == 0:  # Either way, that card is now revealed.
             top_revealed[column] = True
         else:
             bottom_revealed[column] = True
 
+    top_revealed = [True for _ in range(columns)]
+    bottom_revealed = [True for _ in range(columns)]
     output_cards(top_cards, bottom_cards, top_revealed, bottom_revealed)
     hand_score = score_hand(top_cards, bottom_cards)
     if VERBOSE:
@@ -118,7 +132,7 @@ def play_all_hands():
         columns = rng.randint(MIN_COLUMNS, MAX_COLUMNS)
         draws = 2 * columns + rng.randint(1, 3)
         if VERBOSE:
-            print(f"\nStarting hand #{hand+1} with {columns} columns on board.")
+            print(f"\nStarting hand #{hand + 1} with {columns} columns on board.")
         try:
             current_score = play_one_hand(rng, columns, draws)
         except Exception as e:
@@ -130,6 +144,7 @@ def play_all_hands():
 
 
 if __name__ == "__main__":
+    print("Play Nine Solitaire runner, version December 6, 2021, Ilkka Kokkarinen.")
     author_name, author_id = pnp.get_author_info()
-    total_score = play_all_hands()
-    print(f"{author_name}, {author_id}: {total_score}")
+    final_score = play_all_hands()
+    print(f"{author_name}, {author_id}: {final_score}")
